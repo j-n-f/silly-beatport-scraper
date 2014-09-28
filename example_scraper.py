00cd4ac6a7d8eca38faa677ace662184c3ed0643
Johnny Ferguson <johnny@jferg.net>
2014-09-28 02:45:01 -0400

    add more metadata scraping and update example scraper to convert to .flac and tag appropriately
    still todo: load album art into .flac

--- example_scraper.py
+++ example_scraper.py
@@ -1,11 +1,65 @@
+# Note: you must have `flac` and `metaflac` available in your path
+
 from silly_beatport_scraper import *
 import os
+import subprocess
 
 all_files = os.listdir(".")
 all_files = [f_name for f_name in all_files if "wav" in f_name]
 
 my_scraper = SillyBeatportScraper()
 
+print("{} file(s) to scrape".format(len(all_files)))
+print("note: bpm values can be off, better to leave that to analysis software")
+
 for f in all_files:
 	if f[-3:].lower() == "wav":
-		print(my_scraper.meta_from_filename(f))
+		print('-' * 70)
+		print("scraping for: {}".format(f))
+		print('-' * 70)
+		meta = my_scraper.meta_from_filename(f)
+		
+		print('track title:   {}'.format(meta['track_title']))
+		print('track artists: {}'.format(meta['track_artists']))
+		print('genre:         {}'.format(meta['genre']))
+		print('bpm:           {}'.format(meta['bpm']))
+		
+		print('label:         {}'.format(meta['labels']))
+		print('catalogue #:   {}'.format(meta['album_info']['catalogue-number']))
+		print('release name:  {}'.format(meta['release_name']))
+		print('release date:  {}-{}-{}'.format(
+			meta['album_info']['release-date']['year'],
+			meta['album_info']['release-date']['month'],
+			meta['album_info']['release-date']['day'],
+		))
+		try:
+			print('track #:       {}'.format(meta['track_number']))
+		except:
+			print('exception!')
+			print(meta)
+		print('album art url: {}'.format(meta['album_art_url']))
+		print("")
+		
+		
+		print("converting to flac...")
+		args = ['flac', '--verify', '--best'] + [f]
+		#print(args)
+		subprocess.Popen(args).wait()
+		print("")
+		
+		print("tagging...")
+		tags = []
+		tags += ['"Title={}"'.format(meta['track_title'])]
+		tags += ['"Artist={}"'.format(meta['track_artists'])]
+		tags += ['"Album={}"'.format(meta['release_name'])]
+		tags += ['"Year={}"'.format(meta['album_info']['release-date']['year'])]
+		tags += ['"Track={}"'.format(meta['track_number'])]
+		tags += ['"Genre={}"'.format(meta['genre'])]
+		tags += ['"Comment={}"'.format("Catalog #{}".format(meta['album_info']['catalogue-number']))]
+		tags = ["--set-tag={}".format(tag) for tag in tags]
+		tags = ['--remove-all-tags'] + tags
+		
+		args = 'metaflac ' + " ".join(tags) + " " + f.replace('wav', 'flac')
+		#print(args)
+		subprocess.Popen(args, shell=True).wait()
+		print("")
--- silly_beatport_scraper.py
+++ silly_beatport_scraper.py
@@ -51,10 +51,60 @@ class SillyBeatportScraper(object):
 		# used to build a URL to find the track info
 		self.URL_PREFIX = "http://www.beatport.com"
 	
+	def album_info_from_url(self, album_info_url):
+		meta = {}
+		
+		page_data = urllib.urlopen(album_info_url).read()
+		
+		soup = BeautifulSoup(str(page_data))
+		
+		meta['track_listing'] = [
+			{
+				'track_number':
+					int(x.td.findNext('span').contents[0]),
+				'track_title':
+					x.findAll('td')[1].a.findAll('span')[0].contents[0] + " (" +
+					x.findAll('td')[1].a.findAll('span')[1].contents[0] + ")"
+			} for x in
+			soup.h2.findNextSiblings('div')[-1].table.findAll('tr')
+		]
+		
+		meta['album_info_parse'] = soup.find('table', {'class': 'meta-data'})
+		meta['album_info'] = {}
+		for tr in meta['album_info_parse'].findAll('tr'):
+			
+			label = tr.find('td', {'class': 'meta-data-label'}).contents[0]
+			
+			value = None
+			try:
+				value = tr.find('td', {'class': 'meta-data-value'}).contents[0]
+			except TypeError:
+				value = tr.find('td', {'class': 'meta-data-value'}).a.contents[0]
+			
+			if label == 'Release Date':
+				date_particles = value.split('-')
+				
+				meta['album_info']['release-date'] = {}
+				
+				meta['album_info']['release-date']['year'] = int(date_particles[0])
+				meta['album_info']['release-date']['month'] = int(date_particles[1])
+				meta['album_info']['release-date']['day'] = int(date_particles[2])
+			elif label == 'Catalog #':
+				meta['album_info']['catalogue-number'] = value
+			
+		del meta['album_info_parse']
+		
+		#//geo-media.beatport.com/image_size/500x500/8620536.jpg
+		
+		the_span = soup.find('span', {'class': 'artwork'})
+		the_image = the_span.find('img')['data-lazy-load-src']
+		
+		meta['album_art_url'] = the_image.replace('212x212', '500x500')
+		
+		return meta
+	
 	def meta_from_filename(self, filename):
 		meta = {}
-	
-		print("debug: filename: {}".format(filename))
 	
 		track_info = self.RE_EXTRACT.match(filename)
 		
@@ -67,7 +117,7 @@ class SillyBeatportScraper(object):
 		
 		soup = BeautifulSoup(str(page_data))
 		
-		meta['track_title'] = "{} {}".format(
+		meta['track_title'] = "{} ({})".format(
 			soup.h2.contents[0], soup.h2.span.contents[0]
 		) 
 		meta['track_artists'] = [c.contents[0] for c in soup.find(
@@ -91,6 +141,8 @@ class SillyBeatportScraper(object):
 			text="Releases"
 		).findNext('span').a['href']
 		
+		meta.update(self.album_info_from_url(meta['release_info_url']))
+		
 		# TODO
 		#meta['track_number'] = None
 		#meta['catalog_number'] = None
@@ -99,17 +151,28 @@ class SillyBeatportScraper(object):
 		#print(soup.find('span', {'class': 'artists-value h4 fontCondensed'}))
 		
 		# turn lists into comma separated strings
-		meta = {
-			key: str(", ".join(value)) if isinstance(value, list)
-			                           else value
-									   for key, value in meta.items()
-		}
+		meta['track_artists'] = ", ".join(meta['track_artists'])
 		
 		# get rid of escape characters
-		meta = {
-			key: BeautifulSoup(
-				value, convertEntities=BeautifulSoup.XML_ENTITIES
-			).getText() if not isinstance(value, int) 
-			            else value for key, value in meta.items()}
+		
+		for k,v in meta.items():
+			if not isinstance(v, (list, dict, int)):
+				try:
+					meta[k] = BeautifulSoup(
+						v, convertEntities=BeautifulSoup.XML_ENTITIES
+					).getText()
+				except:
+					pass
+		#meta = {
+			#key: BeautifulSoup(
+			#	value, convertEntities=BeautifulSoup.XML_ENTITIES
+			#).getText() if not isinstance(value, int) 
+			#            else value for key, value in meta.items()}
+		
+		for track in meta['track_listing']:
+			if BeautifulSoup(track['track_title'], convertEntities=BeautifulSoup.XML_ENTITIES).getText() == meta['track_title']:
+				meta['track_number'] = track['track_number']
+				del meta['track_listing']
+				break
 		
 		return meta
